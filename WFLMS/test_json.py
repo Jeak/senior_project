6d357946eea464f965f369d5c319974a80dcf7c2
Bgoldie20 <35310340+Bgoldie20@users.noreply.github.com>
2020-05-08 17:21:31 -0700

    Jack finds the error

--- WFLMS/test_json.py
+++ WFLMS/test_json.py
@@ -18,16 +18,21 @@ currentStatus.SRC_NUM = 2   # hardcode for Pi 0
 currentStatus.EMERG_FLG = 1
 currentStatus.FLINE_STAT = 0
 currentStatus.RSRC_STAT = 1
-currentStatus.MGRS_LOC = '10SEG6374048456'
+currentStatus.MGRS_LOC = bytes('10SEG6374048456','utf-8')
 currentStatus.DEST_TYPE = 10
 currentStatus.DEST_NUM = 1
 currentStatus.RX_TIME = time.ctime()
 # Established and calculated upon receive
 # Convert MGRS to lat and lon
-#lat_lon = m.toLatLon(currentStatus.MGRS_LOC)
-#print(lat_lon)
-#print(type(lat_lon))
-#currentStatus.LAT = lat_lon[1]
-#currentStatus.LON = lat_lon[2]
+try:
+    lat_lon = m.toLatLon(currentStatus.MGRS_LOC)
+except mgrs.core.RTreeError:
+    print('RTree Error')
 
+print(lat_lon)
+print(type(lat_lon))
+currentStatus.LAT = lat_lon[1]
+currentStatus.LON = lat_lon[2]
+
+print('Dump to console:')
 currentStatus.dump_to_console()
